c93949fd802f286910cdcb30042371b9fe167e6e
Raj Sagar <therajsagar@gmail.com>
2019-01-14 02:11:41 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const  App = () => <h1>Welcome to React Weather App</h1>
+
+ReactDOM.render(<App />, document.getElementById('root'));
